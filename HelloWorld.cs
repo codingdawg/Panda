9dd3da76dd2346f9c6752fe40ce9fbebc5997524
codingdawg <praveenk.reddy@gmail.com>
2016-06-17 15:16:58 -0700

    Adding Try Catch

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,10 +6,17 @@ namespace HelloWorld
     {
         static void Main() 
         {
+            try{
+                
+        
             Console.WriteLine("Hello World!");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
+            }
+            catch(){
+                
+            }
             Console.ReadKey();
         }
     }
